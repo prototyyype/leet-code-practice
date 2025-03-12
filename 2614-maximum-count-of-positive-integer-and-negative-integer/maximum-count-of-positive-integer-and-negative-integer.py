1fbcbdf185ac4cc29656994fb3d8a703b00cea06
Carly <32104953+prototyyype@users.noreply.github.com>
2025-03-12 16:29:03 -0700

    Time: 0 ms (100.00%) | Memory: 18 MB (41.65%) - LeetSync

--- 2614-maximum-count-of-positive-integer-and-negative-integer/maximum-count-of-positive-integer-and-negative-integer.py
+++ 2614-maximum-count-of-positive-integer-and-negative-integer/maximum-count-of-positive-integer-and-negative-integer.py
@@ -1,28 +1,20 @@
+from bisect import bisect_left, bisect_right
+
 class Solution:
     def maximumCount(self, nums: List[int]) -> int:
         # given "nums" - sorted in increasing order
         # return max between numPos and numNeg
         numPos, numNeg = 0,0
 
-
-
-        for i in range(len(nums)):
-            if nums[i] < 0:
-                numNeg += 1
-            elif nums[i] > 0:
-                numPos += 1
-
-
-            # while nums[i] < 0:
-            #     numNeg += 1
-            #     i += 1
-            # while nums[i] == 0:
-            #     i += 1
-            # while nums[i] > 0:
-            #     numPos += 1
-            #     i += 1
-        
-                
-
+        # for i in range(len(nums)):
+        #     if nums[i] < 0:
+        #         numNeg += 1
+        #     elif nums[i] > 0:
+        #         numPos += 1
+ 
+        numNeg = bisect_left(nums, 0)
+        numPos = len(nums) - bisect_right(nums, 0)
 
         return max(numPos, numNeg)
+
+
